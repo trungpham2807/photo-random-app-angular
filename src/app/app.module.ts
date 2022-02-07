07be5c015e5dd50dc10126ce6cc982f863bf5600
leonero2807 <trungpham280797@gmail.com>
2022-02-07 21:50:01 +0700

    random-app

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PhotoShowComponent } from './photo-show/photo-show.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhotoShowComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
